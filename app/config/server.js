99fc3fd5c8e75567229b0327fcacd3c11cc425b8
felipe-zg <felipe_zeba@outlook.com>
2020-07-25 19:01:46 -0300

    starting

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '28fa96e8c9829bba99c243a3322bf15d'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '05bfd057-1a85-4ed7-afac-abd27380bb22'
+};
